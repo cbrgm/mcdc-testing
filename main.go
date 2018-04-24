3d4848194dc71d28d67f294ab98d8b98622b1f28
cbrgm <chris@cbrgm.de>
2018-04-24 19:13:40 +0200

    Added comments

--- main.go
+++ main.go
@@ -7,13 +7,18 @@ import (
 )
 
 func main() {
+
+	// parse cli commands
 	filePtr := flag.String("file", "./table.txt", "The file containing the matrix")
 	flag.Parse()
+
+	// create matrix struct from file
 	grid, err := matrix.FromFile(*filePtr)
 	if err != nil {
 		panic(err)
 	}
 
+	// print the output
 	for i, vector := range matrix.GetRelevant(grid) {
 		fmt.Printf("Testcase #%v : %v\n", i, vector)
 	}
